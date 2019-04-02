3eac383e445c70156ff30e1ec7f8c2677b38c356
Raman Fedaseyeu <werediver@gmail.com>
2019-04-02 23:31:03 +0200

    Make source formatting faster through parallelizing

--- format-hs.sh
+++ format-hs.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/sh -e
 
 HIND='stack exec hindent --'
 SHAS='stack exec stylish-haskell -- -i'
+NPROC="$(getconf _NPROCESSORS_ONLN)"
 
-find app src test -name '*.hs' -exec ${HIND} {} \; -exec ${SHAS} {} \;
+find app src test -name '*.hs' -print0 | \
+  xargs -0 -P "${NPROC}" -I % sh -c "${HIND} % && ${SHAS} %"
